afa7e5de3c80f5e5ade491cda259b787bb77bd76
bridcourtney <brid.courtney@gmail.com>
2020-09-13 10:12:52 +0000

    each site/loction image on index.html has lat & long coordinates, when geocode function geocodeLatLng is invoked on image click the location details are returned on the map by reverse geocoding

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,3 +8,13 @@ $("#autocomplete").keyup(function() {
 });
 
 
+ function myFunction1() {
+    document.getElementById("latlng").value = "40.714224,-73.961452";
+}
+                           
+
+                          
+function myFunction2() {
+    document.getElementById("latlng").value = "54.2762846,-8.5030245";
+}
+                            
