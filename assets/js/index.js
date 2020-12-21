57ac6523e4a5c407cc4618a4e5d422e7192ade10
bridcourtney <brid.courtney@gmail.com>
2020-12-21 16:20:22 +0000

    moved js functions from index.html to index.js

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+
+function myFunction1() {
+document.getElementById("latlng").value = "54.2778667, -8.4998002";
+                            }
+                            
+                            
