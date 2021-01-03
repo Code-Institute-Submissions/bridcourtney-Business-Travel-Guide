ce8c05ed583b0c4bbfaacbdc97953927a0a65067
bridcourtney <brid.courtney@gmail.com>
2021-01-03 17:36:31 +0000

    Changed Layout of Index page

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,5 +2,18 @@
 function myFunction1() {
 document.getElementById("latlng").value = "54.2778667, -8.4998002";
                             }
-                            
-                            
+ function myFunction2() {
+    document.getElementById("latlng").value = "50.041761,8.3532641";
+    }
+function myFunction3() {
+    document.getElementById("latlng").value = "19.062803, 72.7648663";
+    }
+function myFunction4() {
+    document.getElementById("latlng").value = "42.3001635, -87.8930871";
+    }
+function myFunction5() {
+    document.getElementById("latlng").value = "48.8499675, 2.2019839";
+    }
+function myFunction6() {
+    document.getElementById("latlng").value = "-22.8958242, -43.7197573";
+    }                                                      
--- assets/js/maps.js
+++ assets/js/maps.js
@@ -2,100 +2,27 @@ let map;
 let places;
 let infoWindow;
 let markers = [];
-let autocomplete;
 let searchFor;
-const countryRestrict = { country: "us" };
 const MARKER_PATH =
   "https://developers.google.com/maps/documentation/javascript/images/marker_green";
 const hostnameRegexp = new RegExp("^https?://.+?/");
 
-//Country choice is default to all in #country.  #country dropdown is hidden in index.html
-const countries = {
-  au: {
-    center: { lat: -25.3, lng: 133.8 },
-    zoom: 4
-  },
-  br: {
-    center: { lat: -14.2, lng: -51.9 },
-    zoom: 3
-  },
-  ca: {
-    center: { lat: 62, lng: -110.0 },
-    zoom: 3
-  },
-  fr: {
-    center: { lat: 46.2, lng: 2.2 },
-    zoom: 5
-  },
-  de: {
-    center: { lat: 51.2, lng: 10.4 },
-    zoom: 5
-  },
-  mx: {
-    center: { lat: 23.6, lng: -102.5 },
-    zoom: 4
-  },
-  nz: {
-    center: { lat: -40.9, lng: 174.9 },
-    zoom: 5
-  },
-  it: {
-    center: { lat: 41.9, lng: 12.6 },
-    zoom: 5
-  },
-  za: {
-    center: { lat: -30.6, lng: 22.9 },
-    zoom: 5
-  },
-  es: {
-    center: { lat: 40.5, lng: -3.7 },
-    zoom: 5
-  },
-  pt: {
-    center: { lat: 39.4, lng: -8.2 },
-    zoom: 6
-  },
-  us: {
-    center: { lat: 37.1, lng: -95.7 },
-    zoom: 3
-  },
-  uk: {
-    center: { lat: 54.8, lng: -4.6 },
-    zoom: 5
-  }
-};
-//END - Country choice is default to all in #country.  #country dropdown is hidden in index.html
-//Initialize Map------------------------------------
+
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 2,
     center: { lat: 15, lng: 0 },
-    mapTypeControl: false,
-    panControl: false,
-    zoomControl: false,
-    streetViewControl: false
+    mapTypeControl: true,
+    panControl: true,
+    zoomControl: true,
+    streetViewControl: true
   });
   infoWindow = new google.maps.InfoWindow({
     content: document.getElementById("info-content")
   });
-  // Create the autocomplete object and associate it with the UI input control.
-  // Restrict the search to the default country, and to place type "cities".
-  autocomplete = new google.maps.places.Autocomplete(
-    document.getElementById("autocomplete"),
-    {
-      types: ["(cities)"],
-      
-    }
-  );
+
   places = new google.maps.places.PlacesService(map);
-  autocomplete.addListener("place_changed", onPlaceChanged);
-  // Add a DOM event listener to react when the user selects a country.
-  document
-  //Country choice is default to all in #country.  #country dropdown is hidden in index.html
-    .getElementById("country")
-    .addEventListener("change", setAutocompleteCountry);
-//END - Country choice is default to all in #country.  #country dropdown is hidden in index.html
-  
+
 //Reverse Geocode initiated onclick #submit---------------------------
 document.getElementById('submit').addEventListener('click', function() {
       clearResults();
@@ -118,9 +45,11 @@ function geocodeLatLng(geocoder, map, infowindow) {
     if (status === "OK") {
       if (results[0]) {
         map.setZoom(11);
+        map.setCenter(latlng);
         const marker = new google.maps.Marker({
           position: latlng,
-          map: map
+          map: map,
+          
         });
         infowindow.setContent(results[0].formatted_address);
         infowindow.open(map, marker);
@@ -131,23 +60,6 @@ function geocodeLatLng(geocoder, map, infowindow) {
       window.alert("Geocoder failed due to: " + status);
     }
   });
-}
-
-   
-  
-
-
-// When the user selects a city, get the place details for the city and
-// zoom the map in on the city.
-function onPlaceChanged() {
-  const place = autocomplete.getPlace();
-
-  if (place.geometry) {
-    map.panTo(place.geometry.location);
-    map.setZoom(15);
-  } else {
-    document.getElementById("autocomplete").placeholder = "Enter a city";
-  }
 }
 
 //Nearby place type search by click---------------------------
@@ -216,23 +128,7 @@ function clearMarkers() {
   }
   markers = [];
 }
-//Country choice is default to all in #country.  #country dropdown is hidden in index.html
-// Also center and zoom the map on the given country.
-function setAutocompleteCountry() {
-  const country = document.getElementById("country").value;
-//Country choice is default to all in #country.  #country dropdown is hidden in index.html
-  if (country == "all") {
-    autocomplete.setComponentRestrictions({ country: [] });
-    map.setCenter({ lat: 15, lng: 0 });
-    map.setZoom(2);
-  } else {
-    autocomplete.setComponentRestrictions({ country: country });
-    map.setCenter(countries[country].center);
-    map.setZoom(countries[country].zoom);
-  }
-  clearResults();
-  clearMarkers();
-}
+
 //END - Country choice is default to all in #country.  #country dropdown is hidden in index.html
 function dropMarker(i) {
   return function() {
@@ -339,4 +235,3 @@ function buildIWContent(place) {
     document.getElementById("iw-website-row").style.display = "none";
   }
 }
-    
