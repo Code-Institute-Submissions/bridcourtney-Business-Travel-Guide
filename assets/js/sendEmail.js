1926efb1d7ec90ccc2867e7e20cf81ebf3131f46
bridcourtney <brid.courtney@gmail.com>
2021-01-06 10:25:30 +0000

    redirected in mainpage on successful completion of contact us form

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -7,7 +7,7 @@ function sendMail(contactForm) {
     .then(
         function(_response) {
             alert('Email sent successfully');
-           
+           window.location.href=("https://8000-a31cd9b5-db9e-4a8a-9c5e-b0b909895b79.ws-eu03.gitpod.io/");
         },
         function(_error) {
             alert('email not sent. Please try again');
